e3613bcb0f01406f903a2b83ae28df025c95915c
WesleyBortoloso <wesleybortolosocco@gmail.com>
2026-02-03 19:31:10 -0300

    chore: setup swagger

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('FPS Ranking API')
+    .setDescription('API para processamento de logs de partidas e rankings')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'Token',
+        description: 'Use: Bearer SECRET-TOKEN',
+      },
+      'bearer',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
